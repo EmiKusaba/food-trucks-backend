e855ee8c2d9c3c454fc2e6cee0e8f31bcb96b330
EmiKusaba <eeemi615@gmail.com>
2020-06-27 18:06:23 -0500

    Get category name with getTruckById

--- controllers/modules/trucks/getTruckById.js
+++ controllers/modules/trucks/getTruckById.js
@@ -3,11 +3,11 @@ const connection = require("../../../postgres/connection");
 const getTruckById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  connection.query("SELECT * FROM trucks WHERE id = $1", [id], (err, result) => {
+  connection.query("select t.*, c.name as category from trucks t join truck_categories tc on t.id = tc.truck_id join categories c on tc.category_id = c.id where t.id = $1", [id], (err, result) => {
     if(err) {
       return res.status(500).send(`Error: ${err}`);
     }
-    
+
     return res.json(result.rows);
   })
 };
